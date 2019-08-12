b6fe06f2e8ce1036f7123a16c52a233735f0cf02
Sarah Townson <26084137+Isioviel@users.noreply.github.com>
2019-08-12 16:35:03 +0100

    Create rgb_led.py

---
+++ rgb_led.py
@@ -0,0 +1,15 @@
+from microbit import *
+
+while True:
+    pin0.write_digital(1)
+    sleep(500)
+    pin1.write_digital(1)
+    sleep(500)
+    pin2.write_digital(1)
+    sleep(500)
+    pin0.write_digital(0)
+    sleep(500)
+    pin1.write_digital(0)
+    sleep(500)
+    pin2.write_digital(0)
+    sleep(500)
